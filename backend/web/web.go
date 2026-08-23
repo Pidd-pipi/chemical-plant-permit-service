e61ba4e141f90b002fc718d892a0485f8ece993a
Pidd-pipi <Pidd-pipi@users.noreply.github.com>
2026-08-23 15:47:55 +0800

    feat: initial import

---
+++ backend/web/web.go
@@ -0,0 +1,30 @@
+package web
+
+import (
+	"embed"
+	"net/http"
+	"sync"
+	"time"
+)
+
+//go:embed index.html app.js
+var files embed.FS
+
+var (
+	refreshMu     sync.Mutex
+	lastRefreshed time.Time
+)
+
+// Handler 返回内置页面并记录最近一次刷新时间。
+func Handler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" && r.URL.Path != "/app.js" {
+		http.NotFound(w, r)
+		return
+	}
+	refreshMu.Lock()
+	lastRefreshed = time.Now()
+	stamp := lastRefreshed.UTC().Format(time.RFC3339)
+	refreshMu.Unlock()
+	w.Header().Set("X-Refreshed-At", stamp)
+	http.FileServer(http.FS(files)).ServeHTTP(w, r)
+}
